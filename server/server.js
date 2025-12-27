96855067ff533825001ef4edf4e2bdfc0c715749
﻿Andy Phan <andyyyphan@gmail.com>
2025-12-26 21:07:10 -0500

    Add cors() middleware and jobsRouter to server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 import express from "express";
+import cors from "cors";
+import { jobsRouter } from "./routes/jobs.js";
 // import session from "express-session";
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+app.use(cors());
+
+app.use("/api/jobs", jobsRouter);
 
 app
   .listen(PORT, () => {
