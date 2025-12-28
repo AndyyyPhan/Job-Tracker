aa377e5aa570ca967d1d526b952ba00cd04ebb33
﻿Andy Phan <andyyyphan@gmail.com>
2025-12-28 02:50:42 -0500

    Add update and delete routes for jobs

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { getJobs, addJob } from "../controllers/jobsController.js";
+import { getJobs, addJob, updateJob, deleteJob } from "../controllers/jobsController.js";
 
 export const jobsRouter = express.Router();
 
 jobsRouter.get("/", getJobs);
 jobsRouter.post("/", addJob);
+jobsRouter.put("/:id", updateJob);
+jobsRouter.delete("/:id", deleteJob);
