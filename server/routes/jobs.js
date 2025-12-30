85e6b27a17c9d84481bf4d435bdc888214cee4cb
Andy Phan <andyyyphan@gmail.com>
2025-12-30 09:41:29 -0500

    Add requireAuth() middleware and apply it to all job routes

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { getJobs, addJob, updateJob, deleteJob } from "../controllers/jobsController.js";
+import { requireAuth } from "../middleware/requireAuth.js";
 
 export const jobsRouter = express.Router();
+
+jobsRouter.use(requireAuth);
 
 jobsRouter.get("/", getJobs);
 jobsRouter.post("/", addJob);
