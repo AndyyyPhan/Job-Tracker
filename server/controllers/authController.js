d881cd8a337100aa72802938db1e625e0d2f99f1
Andy Phan <andyyyphan@gmail.com>
2025-12-30 08:52:16 -0500

    add registerUser() to authController

---
+++ server/controllers/authController.js
@@ -0,0 +1,62 @@
+import pool from "../db.js";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+
+export async function registerUser(req, res) {
+  let { username, email, password } = req.body;
+
+  username = username.trim();
+  email = email.trim();
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ error: "All fields are required." });
+  }
+
+  if (!/^[a-zA-Z0-9_-]{1,20}$/.test(username)) {
+    return res.status(400).json({
+      error:
+        "Username must be 1–20 characters, using letters, numbers, _ or -.",
+    });
+  }
+
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ error: "Invalid email format." });
+  }
+
+  if (
+    !/[A-Z]/.test(password) ||
+    !/[a-z]/.test(password) ||
+    !/[0-9]/.test(password)
+  ) {
+    return res.status(400).json({
+      error: "Password must contain uppercase, lowercase, and number.",
+    });
+  }
+
+  try {
+    const existingUserResult = await pool.query(
+      `SELECT * FROM users WHERE username = $1 OR email = $2 LIMIT 1`,
+      [username, email]
+    );
+
+    const existingUser = existingUserResult.rows[0];
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ error: "Username or email already in use." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const result = await pool.query(
+      `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, username, email`,
+      [username, email, hashedPassword]
+    );
+
+    req.session.userId = result.rows[0].id;
+    res.status(201).json({ message: "User registered.", user: result.rows[0] });
+  } catch (err) {
+    console.error("Registration error:", err.message);
+    res.status(500).json({ error: "Registration failed. Please try again." });
+  }
+}
