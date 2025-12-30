69a65bae629c75144bc97ef560ac454f9ad829c2
Andy Phan <andyyyphan@gmail.com>
2025-12-30 08:51:38 -0500

    Include authRouter in server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 import express from "express";
 import cors from "cors";
 import { jobsRouter } from "./routes/jobs.js";
-// import session from "express-session";
+import { authRouter } from "./routes/auth.js";
+import session from "express-session";
+import "dotenv/config";
 
 const app = express();
 const PORT = 8000;
+const secret = process.env.SESSION_SECRET;
 
 app.use(express.json());
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    },
+  })
+);
 app.use(cors());
 
 app.use("/api/jobs", jobsRouter);
+app.use("/api/auth", authRouter);
 
 app
   .listen(PORT, () => {
